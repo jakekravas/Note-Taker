2679dd58775587f57ddf13eb52f72b3e1d1d2870
Jake Kravas <jakekravas7@gmail.com>
2020-01-25 20:35:51 -0700

    updated API and HTML routes. Also changed var variables to let in index.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,11 @@
-let journalData = require("../journal.json");
-let dbData = require("../db/db.json");
+let noteData = require("../db/db.json");
 
 module.exports = function(app){
-    app.get("/api/journal", (req, res) => {
-        res.json(journalData);
+    app.get("/api/notes", (req, res) => {
+        res.json(noteData);
     });
-
-    app.get("/api/db", (req, res) => {
-        res.json(dbData);
+    
+    app.post("/api/notes", (req, res) => {
+        noteData.push(req.body);
     });
 }
