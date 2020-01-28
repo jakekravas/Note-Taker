f69a0dc172707ae17e001d0327684b3ed90b147e
Jake Kravas <jakekravas7@gmail.com>
2020-01-27 18:27:38 -0700

    added line of code to fix syntax error when running on node

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 1006;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
